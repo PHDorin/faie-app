55ab4bf67b1d09c938d93ba6499ff6ee20c066b4
philipdorin <Gomi92(@>
2018-01-21 21:39:15 -0800

    Add screenn switching between normal and mobile screens

---
+++ src/Normal/NormalView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class NormalView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                this is NORMAL size
+            </div>
+        )
+    }
+}
---
+++ src/XS/XSView.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+export default class XSView extends Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        return (
+            <div>
+                this is XS size
+            </div>
+        )
+    }
+}
